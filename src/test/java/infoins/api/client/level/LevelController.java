9edefd2509e522b9cd62f3af403a87168781802d
outsource.minura <outsource.minura@informaticsint.com>
2020-11-02 16:26:49 +0530

    Update LevelController

--- src/test/java/infoins/api/client/level/LevelController.java
+++ src/test/java/infoins/api/client/level/LevelController.java
@@ -13,8 +13,14 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class LevelController extends BaseClass {
     String baseURL;
-    String createEndPoint ="";
+    String createEndPoint ="/sales/levels";
+    String updatedEndPoint= "/sales/levels";
+    String getLevelIdEndPoint = "/sales/levels/{id}";
+    String deleteLevelIdEndPoint="/sales/levels/{id}";
+    String getAllWithPaginationEndPoint= "/sales/levels/all/pagination";
+    String getBulkEndPoint="/sales/levels/bulk";
 
+    //API to create a Level
     @Test(priority = 1)
     public void createLevelValidTest() throws IOException {
         String createEndPoint="/app-email-configs";
@@ -50,4 +56,157 @@ public class LevelController extends BaseClass {
                 .body("error", equalTo("Bad Request"));
     }
 
+    //API to update an existing Level
+    @Test(priority = 2)
+    public void updateLevelValidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .body(getGeneratedString("\\umbrella\\"+"account-modify-account-invalid.json"))
+                .when()
+                .post(updatedEndPoint)
+                .then()
+                .assertThat().statusCode(200)
+                .and()
+                .body("message", equalTo("Data updated successfully"));
+    }
+    @Test
+    public void updateLevelInvalidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .body(getGeneratedString("\\client\\"+"account-modify-account-invalid.json"))
+                .when()
+                .post(updatedEndPoint)
+                .then()
+                .assertThat().statusCode(400)
+                .and()
+                .body("error", equalTo("Bad Request"));
+    }
+
+    //API to retrieve a Level by LevelId
+    @Test(priority = 3)
+    public void getLevelIdLevelValidTest() throws IOException {
+        int id = 1;
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(getLevelIdEndPoint, id)
+                .then()
+                .assertThat().statusCode(200);
+    }
+    @Test
+    public void getLevelIdLevelInvalidTest() throws IOException {
+        String id = "Id";
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(getLevelIdEndPoint, id)
+                .then()
+                .assertThat().statusCode(400)
+                .and()
+                .body("error", equalTo("Bad Request"));
+
+    }
+
+    //API to delete a Level by LevelId
+    @Test(priority = 4)
+    public void deleteLevelValidTest() throws IOException{
+        int id = 1;
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete(deleteLevelIdEndPoint, id)
+                .then()
+                .assertThat().statusCode(200)
+                .and()
+                .body("message", equalTo("Data deleted successfully"));
+    }
+    @Test
+    public void deleteLevelInvalidTest() throws IOException{
+        String id = "id";
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete(deleteLevelIdEndPoint, id)
+                .then()
+                .assertThat().statusCode(400)
+                .and()
+                .body("error", equalTo("Bad Request"));
+    }
+
+    //API to retrieve all Levels with Pagination
+    @Test(priority = 5)
+    public void getAllWithPaginationLevelValidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .queryParam("pageNo", 0)
+                .queryParam("pageSize", 10)
+                .queryParam("sortBy", "branchId")
+                .when()
+                .get(getAllWithPaginationEndPoint)
+                .then()
+                .assertThat().statusCode(200);
+    }
+    @Test
+    public void getAllWithPaginationLevelInvalidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .queryParam("pageNo", 5)
+                .queryParam("pageSize", 10)
+                .queryParam("sortBy", "branchId")
+                .when()
+                .get(getAllWithPaginationEndPoint)
+                .then()
+                .assertThat().statusCode(400)
+                .and()
+                .body("error", equalTo("Bad Request"));
+    }
+
+    //API to retrieve all Levels
+    @Test(priority = 6)
+    public void viewAllLevelValidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(getBulkEndPoint)
+                .then()
+                .assertThat().statusCode(200);
+    }
 }
