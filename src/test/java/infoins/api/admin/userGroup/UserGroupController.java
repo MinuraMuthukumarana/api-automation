44e91e4884faf1d8860d3064d38d25ddc4fa9709
outsource.minura <outsource.minura@informaticsint.com>
2021-02-08 10:16:09 +0530

    update UserGroupController

--- src/test/java/infoins/api/admin/userGroup/UserGroupController.java
+++ src/test/java/infoins/api/admin/userGroup/UserGroupController.java
@@ -2,12 +2,17 @@ package infoins.api.admin.userGroup;
 
 import infoins.AccessTokenHolder;
 import infoins.BaseClass;
+import infoins.ExcelDataReader;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.ITestContext;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 import static io.restassured.RestAssured.baseURI;
@@ -26,10 +31,6 @@ public class UserGroupController extends BaseClass {
     String updateEndPoint ="/user-groups";
     String getOneEndPoint = "/user-groups/{id}";
     String getAllWithPaginationEndPoint = "/user-groups/all/pagination";
-    String getAllGroupBranchesByGroupIdNameOrReferenceEndPoint="/user-groups/branches/{search-criteria}";
-    String getAllGroupLimitsByGroupIdNameOrReferenceEndPoint = "/user-groups/limits/{search-criteria}";
-
-
 
     @Test(priority = 1)
     public void createUserGroupValidTest() throws IOException{
@@ -47,23 +48,6 @@ public class UserGroupController extends BaseClass {
                 .assertThat().statusCode(201)
                 .and()
                 .body("message", equalTo("Data added successfully"));
-    }
-    @Test
-    public void createUserGroupInvalidTest() throws IOException{
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .body(getGeneratedString("\\admin\\"+"create-user-group-invalid.json"))
-                .when()
-                .post(createEndPoint)
-                .then()
-                .assertThat().statusCode(400)
-                .and()
-                .body("error", equalTo("Data not found."));
     }
 
     @Test(priority = 2)
@@ -155,43 +139,7 @@ public class UserGroupController extends BaseClass {
                 .body("message", equalTo("Data updated successfully"));
 
     }
-    @Test
-    public void updateUserGroupInvalidTest() throws IOException{
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "  \"countryCode\": \"SL\",\n" +
-                        "  \"groupId\": 10,\n" +
-                        "  \"groupLimits\": [\n" +
-                        "    {\n" +
-                        "      \"branchId\": 10,\n" +
-                        "      \"classId\": 1,\n" +
-                        "      \"functionId\": 1,\n" +
-                        "      \"groupLimitId\": 1,\n" +
-                        "      \"levelConfigId\": 1,\n" +
-                        "      \"maxLimit\": 100,\n" +
-                        "      \"minLimit\": 10,\n" +
-                        "      \"productId\": 2\n" +
-                        "    }\n" +
-                        "  ],\n" +
-                        "  \"groupRefId\": \"035b9a6e-59f4-4ac0-9e11-e264c86ed43d\",\n" +
-                        "  \"grpBranchIds\": [\n" +
-                        "    10\n" +
-                        "  ]\n" +
-                        "}")
-                .when()
-                .put(updateEndPoint)
-                .then()
-                .assertThat().statusCode(400)
-                .and()
-                .body("error", equalTo("Bad Request"));
 
-    }
 
     @Test(priority = 4)
     public void getOneUserGroupValidTest() throws IOException{
@@ -235,76 +183,61 @@ public class UserGroupController extends BaseClass {
 
     }
 
+    //Invalid scenarios for CreateUserGroup
+    @DataProvider(name = "createUserGroupInvalidTest")
+    @Parameters({"UserGroupController"})
+    public Iterator<Object[]> getUserGroupCreateTestData(ITestContext context) throws IOException {
 
-//    @Test(priority = 5)
-//    public void getAllGroupBranchesByGroupIdNameOrReferenceUserGroupValidTest() throws IOException{
-//       // String search-criteria ="";
-//        baseURL = getURL();
-//        baseURI = baseURL;
-//        Response response=
-//        given()
-//                .header("accept", "*/*")
-//                .header("authorization", AccessTokenHolder.access_token)
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(getAllGroupBranchesByGroupIdNameOrReferenceEndPoint)
-//                .then()
-//                .assertThat().statusCode(200)
-//                .and().extract().response();
-//
-//        String jsonStr = response.getBody().asString();
-//        System.out.println("search-criteria Data List: " + jsonStr);
-//    }
-//    @Test
-//    public void getAllGroupBranchesByGroupIdNameOrReferenceUserGroupInvalidTest() throws IOException{
-//        baseURL = getURL();
-//        baseURI = baseURL;
-//
-//        given()
-//                .header("accept", "*/*")
-//                .header("authorization", AccessTokenHolder.access_token)
-//                .contentType(ContentType.JSON)
-//                .queryParam("search-criteria", "Invalid")
-//                .when()
-//                .get(getAllGroupBranchesByGroupIdNameOrReferenceEndPoint)
-//                .then()
-//                .assertThat().statusCode(400)
-//                .and()
-//                .body("error", equalTo("Bad Request"));
-//
-//    }
-//
-//    @Test(priority = 6)
-//    public void  getAllGroupLimitsByGroupIdNameOrReferenceEndPointValidTest() throws IOException{
-//        baseURL = getURL();
-//        baseURI = baseURL;
-//        given()
-//                .header("accept", "*/*")
-//                .header("authorization", AccessTokenHolder.access_token)
-//                .contentType(ContentType.JSON)
-//                .queryParam("search-criteria", "")
-//                .when()
-//                .get(getAllGroupLimitsByGroupIdNameOrReferenceEndPoint)
-//                .then()
-//                .assertThat().statusCode(200);
-//
-//    }
-//    @Test
-//    public void  getAllGroupLimitsByGroupIdNameOrReferenceEndPointInvalidTest() throws IOException{
-//        baseURL = getURL();
-//        baseURI = baseURL;
-//
-//        given()
-//                .header("accept", "*/*")
-//                .header("authorization", AccessTokenHolder.access_token)
-//                .contentType(ContentType.JSON)
-//                .queryParam("search-criteria", "Invalid")
-//                .when()
-//                .get(getAllGroupBranchesByGroupIdNameOrReferenceEndPoint)
-//                .then()
-//                .assertThat().statusCode(400)
-//                .and()
-//                .body("error", equalTo("Bad Request"));
-//
-//    }
+        String dataFile = context.getCurrentXmlTest().getParameter("UserGroupController");
+        Iterator<Object[]> iterator = ExcelDataReader.excelDataReader(0,"\\AdminExcel\\UserGroupController.xlsx");
+        return iterator;
+    }
+    @Test(dataProvider = "createUserGroupInvalidTest")
+    public void CreateUserGroupControllerInvalid(Integer statusCode, String schema, String message) throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .header("CountryId", 1)
+                .contentType(ContentType.JSON)
+                .body(schema)
+                .when()
+                .post(createEndPoint)
+                .then()
+                .assertThat().statusCode(statusCode)
+                .and()
+                .body("error_description", equalTo(message));
+    }
+
+    //Invalid scenarios for UpdateUserGroup
+    @DataProvider(name = "UpdateUserGroupInvalidTest")
+    @Parameters({"UserGroupController"})
+    public Iterator<Object[]> getUserGroupUpdateTestData(ITestContext context) throws IOException {
+
+        String dataFile = context.getCurrentXmlTest().getParameter("UserGroupController");
+        Iterator<Object[]> iterator = ExcelDataReader.excelDataReader(1,"\\AdminExcel\\UserGroupController.xlsx");
+        return iterator;
+    }
+    @Test(dataProvider = "UpdateUserGroupInvalidTest")
+    public void UpdateUserGroupControllerInvalid(Integer statusCode, String schema, String message) throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .header("CountryId", 1)
+                .contentType(ContentType.JSON)
+                .body(schema)
+                .when()
+                .put(updateEndPoint)
+                .then()
+                .assertThat().statusCode(statusCode)
+                .and()
+                .body("error_description", equalTo(message));
+    }
+
+
 }
