92e4d82050f057340fc69c229854556c157a3f9d
outsource.minura <outsource.minura@informaticsint.com>
2021-02-15 16:29:46 +0530

    update UserController

--- src/test/java/infoins/api/admin/user/UserController.java
+++ src/test/java/infoins/api/admin/user/UserController.java
@@ -2,11 +2,16 @@ package infoins.api.admin.user;
 
 import infoins.AccessTokenHolder;
 import infoins.BaseClass;
+import infoins.ExcelDataReader;
 import io.restassured.http.ContentType;
-import org.testng.annotations.BeforeTest;
+import io.restassured.response.Response;
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
@@ -20,7 +25,7 @@ import static org.hamcrest.Matchers.equalTo;
  *  */
 
 public class UserController extends BaseClass {
-
+    int x;
     String baseURL;
     String createEndPoint = "/users";
     String modifyEndPoint = "/users";
@@ -47,44 +52,62 @@ public class UserController extends BaseClass {
                 .assertThat().statusCode(201);
 
     }
-    @Test
-    public void createInvalidDataType1Test() throws IOException {
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .body(getGeneratedString("\\admin\\"+"create-user-datatpe1-invalid.json"))
-                .when()
-                .post(createEndPoint)
-                .then()
-                .assertThat().statusCode(400)
-                .and()
-                .body("error", equalTo("Bad Request"));
-
-    }
-    @Test
-    public void createInvalidDataType2Test() throws IOException {
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .body(getGeneratedString("\\admin\\"+"create-user-datatpe2-invalid.json"))
-                .when()
-                .post(createEndPoint)
-                .then()
-                .assertThat().statusCode(400)
-                .and()
-                .body("error", equalTo("Bad Request"));
-
-    }
 
     @Test(priority = 2)
+    public void getAllWithPaginationValidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+        Response response =
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .header("CountryId", 1)
+                .contentType(ContentType.JSON)
+                .queryParam("pageNo", 1)
+                .queryParam("pageSize", 100)
+                .queryParam("sortBy", "userId")
+                .when()
+                .get(getAllWithPaginationEndPoint)
+                .then()
+                .assertThat().statusCode(200)
+                .and().extract().response();
+
+        String jsonStr = response.getBody().asString();
+        System.out.println("Data List: " + jsonStr);
+
+        int size = response.jsonPath().getList("data.userId").size();
+        System.out.println("Data Size: " + size);
+
+        List<Integer> ids = response.jsonPath().getList("data.userId");
+        x= ids.get(size-1);
+        System.out.println("Last index:" +x);
+        for (Integer i : ids) {
+            System.out.print(i);
+        }
+
+    }
+    @Test
+    public void getAllWithPaginationSortByInvalidTest() throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .header("CountryId", 1)
+                .contentType(ContentType.JSON)
+                .queryParam("pageNo", -1)
+                .queryParam("pageSize", -100)
+                .queryParam("sortBy", "userId")
+                .when()
+                .get(getAllWithPaginationEndPoint)
+                .then()
+                .assertThat().statusCode(400).and()
+                .body("error_description", equalTo("Page index must not be less than zero!"));
+
+
+    }
+
+    @Test(priority = 3)
     public void modifyValidTest() throws IOException {
         baseURL = getURL();
         baseURI = baseURL;
@@ -137,11 +160,12 @@ public class UserController extends BaseClass {
 
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void getOneValidTest() throws IOException {
-        int id = 1;
+        int id = x;
         baseURL = getURL();
         baseURI = baseURL;
+        Response response =
         given()
                 .header("accept", "*/*")
                 .header("authorization", AccessTokenHolder.access_token)
@@ -150,12 +174,16 @@ public class UserController extends BaseClass {
                 .when()
                 .get(getOneEndPoint, id)
                 .then()
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(200)
+                .and().extract().response();
+
+        String jsonStr = response.getBody().asString();
+        System.out.println("GetOne UserId List: " + jsonStr);
 
     }
     @Test
     public void getOneInvalidTest() throws IOException {
-        String id = "id";
+        int id = -1;
         baseURL = getURL();
         baseURI = baseURL;
         given()
@@ -168,48 +196,13 @@ public class UserController extends BaseClass {
                 .then()
                 .assertThat().statusCode(400)
                 .and()
-                .body("error", equalTo("Bad Request"));
-
-    }
-
-    @Test(priority = 4)
-    public void deleteValidTest() throws IOException {
-        int id = 1;
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .when()
-                .delete(deleteEndPoint, id)
-                .then()
-                .assertThat().statusCode(200);
-
-    }
-    @Test
-    public void deleteInvalidTest() throws IOException {
-        String id = "id";
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .when()
-                .delete(deleteEndPoint, id)
-                .then()
-                .assertThat().statusCode(400)
-                .and()
-                .body("error", equalTo("Bad Request"));
+                .body("message", equalTo("Data not found"));
 
     }
 
     @Test(priority = 5)
-    public void deleteAllValidTest() throws IOException {
-        String ids = "1,2";
+    public void deleteValidTest() throws IOException {
+        int id = x;
         baseURL = getURL();
         baseURI = baseURL;
         given()
@@ -218,14 +211,16 @@ public class UserController extends BaseClass {
                 .header("CountryId", 1)
                 .contentType(ContentType.JSON)
                 .when()
-                .delete(deleteAllEndPoint, ids)
+                .delete(deleteEndPoint, id)
                 .then()
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(200)
+                .and()
+                .body("message", equalTo("Data deleted successfully"));
 
     }
     @Test
-    public void deleteAllInvalidTest() throws IOException {
-        int ids = 1;
+    public void deleteInvalidTest() throws IOException {
+        int id = -1;
         baseURL = getURL();
         baseURI = baseURL;
         given()
@@ -234,54 +229,15 @@ public class UserController extends BaseClass {
                 .header("CountryId", 1)
                 .contentType(ContentType.JSON)
                 .when()
-                .delete(deleteAllEndPoint, ids)
+                .delete(deleteEndPoint, id)
                 .then()
                 .assertThat().statusCode(400)
                 .and()
-                .body("error", equalTo("Bad Request"));
+                .body("message", equalTo("Data not found"));
 
     }
 
     @Test(priority = 6)
-    public void getAllWithPaginationValidTest() throws IOException {
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .queryParam("pageNo", 1)
-                .queryParam("pageSize", 10)
-                .queryParam("sortBy", "bypass")
-                .when()
-                .get(getAllWithPaginationEndPoint)
-                .then()
-                .assertThat().statusCode(200);
-
-    }
-    @Test
-    public void getAllWithPaginationSortByInvalidTest() throws IOException {
-        baseURL = getURL();
-        baseURI = baseURL;
-        given()
-                .header("accept", "*/*")
-                .header("authorization", AccessTokenHolder.access_token)
-                .header("CountryId", 1)
-                .contentType(ContentType.JSON)
-                .queryParam("pageNo", 1)
-                .queryParam("pageSize", 10)
-                .queryParam("sortBy", 1)
-                .when()
-                .get(getAllWithPaginationEndPoint)
-                .then()
-                .assertThat().statusCode(400)
-                .and()
-                .body("error", equalTo("Bad Request"));
-
-    }
-
-    @Test(priority = 7)
     public void getBulkValidTest() throws IOException {
         baseURL = getURL();
         baseURI = baseURL;
@@ -296,5 +252,31 @@ public class UserController extends BaseClass {
                 .assertThat().statusCode(200);
 
     }
+
+    //Invalid scenarios for CreateUser
+    @DataProvider(name = "createUserInvalidTest")
+    public Iterator<Object[]> getUserCreateTestData(ITestContext context) throws IOException {
+        Iterator<Object[]> iterator = ExcelDataReader.excelDataReader(0,"\\AdminExcel\\UserController.xlsx");
+        return iterator;
+    }
+    @Test(dataProvider = "createUserInvalidTest")
+    public void CreateUserControllerInvalid(Integer statusCode, String schema, String message) throws IOException {
+        baseURL = getURL();
+        baseURI = baseURL;
+
+        given()
+                .header("accept", "*/*")
+                .header("authorization", AccessTokenHolder.access_token)
+                .header("CountryId", 1)
+                .contentType(ContentType.JSON)
+                .body(schema)
+                .when()
+                .post(createEndPoint)
+                .then()
+                .assertThat().statusCode(statusCode)
+                .and()
+                .body("error_description", equalTo(message));
+    }
+
 
 }
